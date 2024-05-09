d12378f431f2aa54d7d052043074183c67788c96
Wediah <100675006+Wediah@users.noreply.github.com>
2024-05-09 23:38:29 +0000

    configs projects to run on docker part 3

--- database/migrations/2024_05_07_145711_create_expenses_table.php
+++ database/migrations/2024_05_07_145711_create_expenses_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->string('expense');
+            $table->string('amount');
             $table->timestamps();
         });
     }
